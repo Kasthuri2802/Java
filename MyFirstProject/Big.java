8baf42da13af377dc0fc456a3ba0550e6c3658c8
Kasthuri2802 <kasthurimuthuraj2002@gmail.com>
2023-12-20 10:07:03 +0530

    modify

--- MyFirstProject/Big.java
+++ MyFirstProject/Big.java
@@ -1,14 +1,11 @@
 public class Big {
     public static void main(String[] args) {
-        int a=10,b,c,d;
-        b= ++a +15;
-        c=a-- + --b -10;
-        d=--a+ ++b- --c;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        
+        int a=-10;
+        if(a<0)
+        {
+            a *= -1;
+            System.out.println(a);
 
+        }
     }
 }
